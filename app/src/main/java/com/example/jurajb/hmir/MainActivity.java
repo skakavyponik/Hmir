5812bd7ef5bf5b15c8fdb2392fe016f69aced1e0
MatejBartosovic <matejbart@gmail.com>
2017-02-23 11:39:23 +0100

    Main window

--- app/src/main/java/com/example/jurajb/hmir/MainActivity.java
+++ app/src/main/java/com/example/jurajb/hmir/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.jurajb.hmir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView sensorList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sensorList = (ListView) findViewById(R.id.SensorList);
+        sensorList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getBaseContext(),sensorList.getItemAtPosition(position).toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
